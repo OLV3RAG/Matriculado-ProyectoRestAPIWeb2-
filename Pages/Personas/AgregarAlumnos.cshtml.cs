d881352dfadd4039f30d7fd83bc6d9af79ef18b7
OLV3RAG <daaaayv@gmail.com>
2026-01-02 13:01:09 -0600

    cambios en el codigo

---
+++ Pages/Personas/AgregarAlumnos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Matriculado.Pages.Personas
+{
+    public class AgregarAlumnosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Personas/EditarPersonas.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Matriculado.Pages.Personas
+{
+    public class EditarPersonasModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
